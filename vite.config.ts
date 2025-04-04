1468ccbadedee02e7102e85bd0a469f49a28e541
marcos-aparicio <marcos.aparicio593@gmail.com>
2025-04-04 12:05:51 -0400

    changing app.js to app.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     plugins: [
         tailwindcss(),
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: ["resources/css/app.css", "resources/js/app.ts"],
             refresh: true,
         }),
         vue(),
